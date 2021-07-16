6780d6f2295222a8a02035cc1126f43679f5d637
Happy Omokaro <omohappyboi@gmail.com>
2021-07-16 12:06:10 +0100

    third commit, modification

--- mortage.js
+++ mortage.js
@@ -6,12 +6,30 @@ press=()=>{
   // formular for mortage monthly payment
   let r=R1*0.01
   let value= P1*(((r*((1+r)**N1)))/(((1+r)**N1)-1));
-  let output=document.getElementById('result');
+  let output1=document.getElementById('result');
+  let output2=document.getElementById('output');
+  
   if (value) {
-    output.innerHTML="Your monthly payment will be (N): "+ value.toFixed(2);
+    output2.value=value.toFixed(2);
+    const nam=document.querySelector('#result');
+    nam.innerHTML=''
     
   }
   else{
-    output.innerHTML="Please fill each section correctly!"
+    output1.innerHTML="Please fill each section correctly!";
   }
-}
+};
+// clear all button
+ const clearText=()=>{
+  const name1=document.querySelector('#p');
+  const name2=document.querySelector('#r');
+  const name3=document.querySelector('#n');
+  const name4=document.querySelector('#result');
+  const name5=document.querySelector('#output');
+  name1.value='';
+  name2.value='';
+  name3.value='';
+  name4.innerHTML='';
+  name5.value='';
+
+};
